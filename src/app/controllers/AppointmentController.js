a02812659451a9485dedd38f47032ec369bdc71a
Manoel <manoel.lobo@gmail.com>
2019-07-04 14:23:21 -0300

    user appointment cancelation

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -83,6 +83,30 @@ class AppointmentController {
       content: `New appointment scheduled to ${name} at ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res
+        .status(401)
+        .json({ error: 'Invalid permission for this appointment' });
+    }
+
+    const limitDateToCancel = subHours(appointment.date, 2);
+
+    if (isBefore(limitDateToCancel, new Date())) {
+      return res.status(401).json({
+        error: "You can't cancel an appointment within less than 2 hours",
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
