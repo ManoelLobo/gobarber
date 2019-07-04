e1bf0453d6a5b8cd2dcb184f52722b60609db75e
Manoel <manoel.lobo@gmail.com>
2019-07-04 15:33:21 -0300

    appointment cancelation send mail to provider

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -88,7 +90,9 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [{ model: User, as: 'provider', attributes: ['name', 'email'] }],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res
@@ -107,6 +111,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Appointment canceled',
+      text: 'You have one less appointment :(',
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: '',
+  port: '',
+  secure: false,
+  auth: {
+    user: '',
+    password: '',
+  },
+  default: {
+    from: 'GoBarber App <no-reply@gobarb.er>',
+  },
+};
